6961bc94abc8fc9f91a56a56f83376ea9274cf0b
Ingrid Zippe <ingrid_zippe@brown.edu>
2017-10-21 09:02:28 -0700

    ya

--- app.js
+++ app.js
@@ -25,18 +25,18 @@ passport.serializeUser((user, done) => done(null, user));
 passport.deserializeUser((user, done) => done(null, user));
 
 
-passport.use(new GoogleStrategy({
-    clientID:     process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: process.env.CALLBACK_URL,
-    passReqToCallback   : true
-  },
-  function(request, accessToken, refreshToken, profile, done) {
-    models.User.findOrCreate({ googleId: profile.id }, function (err, user) {
-      return done(err, user);
-    });
-  }
-));
+// passport.use(new GoogleStrategy({
+//     clientID:     process.env.CLIENT_ID,
+//     clientSecret: process.env.CLIENT_SECRET,
+//     callbackURL: process.env.CALLBACK_URL,
+//     passReqToCallback   : true
+//   },
+//   function(request, accessToken, refreshToken, profile, done) {
+//     models.User.findOrCreate({ googleId: profile.id }, function (err, user) {
+//       return done(err, user);
+//     });
+//   }
+// ));
 
 // passport.use(new GoogleStrategy({
 //     clientID:     process.env.CLIENT_ID,
