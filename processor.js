fc52c0887e1d11a4ffea2253047ef4af7037f4f5
natedana <natepdana@gmail.com>
2017-10-21 07:59:39 -0700

    logs for progess

--- processor.js
+++ processor.js
@@ -10,20 +10,16 @@ function processor(file) {
     readImage(file)
     .then(txt=>{
       //FILE TURNED INTO RAW TEXT
-      var example = 'San Fran March 16 Jerry and Beth 2:00 pm'
+      console.log("Image read into text");
       return readText(txt)
     }).then(info=>{
       //TEXT TURNED INTO INFO OBJECT
-      var example = {
-        people: ['Jerry', 'Beth'],
-        time: ['March 16', '2 pm'],
-        place: ['San Francisco','CA']
-      }
+      console.log('Text parsed into information');
       return formEvent(info)
     }).then(evt=>{
       //INFO BECOMES EVENT OBJ FOR GOOGLE
-      var calendarID = 'primary'; //default calendar
-      resolve(evt);
+      console.log("Event created");
+      resolve(evt)
     }).catch(err=>{
       console.log("ERR",err);
       reject(err)
