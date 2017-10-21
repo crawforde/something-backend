5a617641b5548fe98f0d2bb6544b31dc71b4c308
natedana <natepdana@gmail.com>
2017-10-21 07:55:54 -0700

    get rid of deadweight

--- app.js
+++ app.js
@@ -12,10 +12,10 @@ var routes = require('./routes/routes');
 
 // Transform Facebook profile because Facebook and Google profile objects look different
 // and we want to transform them into user objects that have the same set of attributes
-const transformGoogleProfile = (profile) => ({
-  name: profile.displayName,
-  avatar: profile.image.url,
-});
+// const transformGoogleProfile = (profile) => ({
+//   name: profile.displayName,
+//   avatar: profile.image.url,
+// });
 
 // Serialize user into the sessions
 passport.serializeUser((user, done) => done(null, user));
@@ -23,18 +23,18 @@ passport.serializeUser((user, done) => done(null, user));
 // Deserialize user from the sessions
 passport.deserializeUser((user, done) => done(null, user));
 
-passport.use(new GoogleStrategy({
-    clientID:     process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: process.env.CALLBACK_URL,
-    passReqToCallback   : true
-  },
-  function(request, accessToken, refreshToken, profile, done) {
-    User.findOrCreate({ googleId: profile.id }, function (err, user) {
-      return done(err, user);
-    });
-  }
-));
+// passport.use(new GoogleStrategy({
+//     clientID:     process.env.CLIENT_ID,
+//     clientSecret: process.env.CLIENT_SECRET,
+//     callbackURL: process.env.CALLBACK_URL,
+//     passReqToCallback   : true
+//   },
+//   function(request, accessToken, refreshToken, profile, done) {
+//     User.findOrCreate({ googleId: profile.id }, function (err, user) {
+//       return done(err, user);
+//     });
+//   }
+// ));
 // Initialize http server
 var app = express();
 
@@ -52,13 +52,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.get('/auth/google', passport.authenticate('google', { scope: ['profile'] }));
-app.use(auth(passport));
-app.use(routes);
+// app.get('/auth/google', passport.authenticate('google', { scope: ['profile'] }));
+// app.use(auth(passport));
+// app.use(routes);
 
-app.get('/auth/google/callback',
-  passport.authenticate('google', { failureRedirect: '/login' }),
-  (req, res) => res.redirect('/users/' + req.user));
+// app.get('/auth/google/callback',
+//   passport.authenticate('google', { failureRedirect: '/login' }),
+//   (req, res) => res.redirect('/users/' + req.user));
 
 // passport strategy
 passport.use(new LocalStrategy(function(username, password, done) {
@@ -85,18 +85,18 @@ passport.use(new LocalStrategy(function(username, password, done) {
 }));
 
 //ROUTES
-
-app.get('/auth/google',
-  passport.authenticate('google', { scope:
-  	[ 'https://localhost:3000/auth/plus.login',
-  	  'https://localhost:3000/auth/plus.profile.emails.read' ] }
-));
-
-app.get( '/users/register',
-	passport.authenticate( 'google', {
-		successRedirect: '/users',
-		failureRedirect: '/login'
-}));
+//
+// app.get('/auth/google',
+//   passport.authenticate('google', { scope:
+//   	[ 'https://localhost:3000/auth/plus.login',
+//   	  'https://localhost:3000/auth/plus.profile.emails.read' ] }
+// ));
+//
+// app.get( '/users/register',
+// 	passport.authenticate( 'google', {
+// 		successRedirect: '/users',
+// 		failureRedirect: '/login'
+// }));
 
 
 
--- routes/auth.js
+++ routes/auth.js
@@ -13,90 +13,93 @@ module.exports = function (passport) {
   router.use(bodyParser.json());
   router.use(bodyParser.urlencoded({ extended: true }));
 
-  /* Authentication routes */
+  // /* Authentication routes */
+  //
+  // router.get('/login/failure', function(req, res) {
+  //   res.status(401).json({
+  //     success: false,
+  //     error: req.flash('error')[0]
+  //   });
+  // });
+  //
+  // router.post('/login', passport.authenticate('local', {
+  //   successRedirect: '/login/success',
+  //   failureRedirect: '/login/failure',
+  //   failureFlash: true
+  // }));
+  //
+  // router.post('/register', function(req, res, next) {
+  //   const user = new models.User({
+  //     username: req.body.username,
+  //     password: req.body.password
+  //   })
+  //   console.log('USER', user);
+  //   //var params = _.pick(req.body, ["username", "password"]);
+  //   user.save(function(err, user) {
+  //     if (err) {
+  //       res.status(400).json({
+  //         success: false,
+  //         error: err.message
+  //       });
+  //     } else {
+  //       res.json({
+  //         success: true,
+  //         user: user
+  //       });
+  //     }
+  //   });
+  // });
+  //
+  // router.post('/register', function(req, res) {
+  //     const u = new models.User({
+  //       username: req.body.username,
+  //       password: req.body.password
+  //     });
+  //     console.log(u);
+  //     u.save(function(err, user) {
+  //       if (err) {
+  //         console.log(err);
+  //         res.status(500).redirect('/register');
+  //       } else {
+  //         res.json({
+  //           success: true,
+  //           user: user
+  //         });
+  //       }
+  //     });
+  // })
+  //
+  // router.get('/logout', function(req, res) {
+  //   req.logout();
+  //   res.json({
+  //     success: true,
+  //     message: 'logged out.'
+  //   });
+  // });
+  //
+  // router.get('/login/success', function(req, res) {
+  //   var user = _.pick(req.user, 'username', '_id');
+  //   console.log(req.user)
+  //   res.json({
+  //     success: true,
+  //     user: user
+  //   });
+  // });
+  //
 
-  router.get('/login/failure', function(req, res) {
-    res.status(401).json({
-      success: false,
-      error: req.flash('error')[0]
-    });
-  });
-
-  router.post('/login', passport.authenticate('local', {
-    successRedirect: '/login/success',
-    failureRedirect: '/login/failure',
-    failureFlash: true
-  }));
-
-  router.post('/register', function(req, res, next) {
-    const user = new models.User({
-      username: req.body.username,
-      password: req.body.password
-    })
-    console.log('USER', user);
-    //var params = _.pick(req.body, ["username", "password"]);
-    user.save(function(err, user) {
-      if (err) {
-        res.status(400).json({
-          success: false,
-          error: err.message
-        });
-      } else {
-        res.json({
-          success: true,
-          user: user
-        });
-      }
-    });
-  });
-
-  router.post('/register', function(req, res) {
-      const u = new models.User({
-        username: req.body.username,
-        password: req.body.password
-      });
-      console.log(u);
-      u.save(function(err, user) {
-        if (err) {
-          console.log(err);
-          res.status(500).redirect('/register');
-        } else {
-          res.json({
-            success: true,
-            user: user
-          });
-        }
-      });
-  })
-
-  router.get('/logout', function(req, res) {
-    req.logout();
-    res.json({
-      success: true,
-      message: 'logged out.'
-    });
-  });
-
-  router.get('/login/success', function(req, res) {
-    var user = _.pick(req.user, 'username', '_id');
-    console.log(req.user)
-    res.json({
-      success: true,
-      user: user
-    });
-  });
-
-
+  // router.use('/', function(req,res,next){
+  //   if(req.user){
+  //     next();
+  //   }
+  //   else{
+  //     res.status(401).json({
+  //       success: false,
+  //       error: 'Error: Session not found. Please log in to see content.'
+  //     });
+  //   }
+  // });
   router.use('/', function(req,res,next){
-    if(req.user){
-      next();
-    }
-    else{
-      res.status(401).json({
-        success: false,
-        error: 'Error: Session not found. Please log in to see content.'
-      });
-    }
+    res.send('hi there!')
   });
 
   return router;
